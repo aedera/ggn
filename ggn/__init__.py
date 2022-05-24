95e7615b4911c4e9c7dc47e99e36f21fe168ee28
blindcosmos <104645183+blindcosmos@users.noreply.github.com>
2022-05-24 11:28:10 -0300

    Global variable

--- ggn/__init__.py
+++ ggn/__init__.py
@@ -1,4 +1,9 @@
 __version__ = '0.1.0'
 
+import os
+
 # minimum value allow in diagonal for ensuring positive definitiveness
 PD_THR = 0.01
+
+basename = os.path.dirname(__file__)
+TOYGRAPH = os.path.join(basename, 'toy.npz')
