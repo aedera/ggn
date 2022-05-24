fc30c4a573b35e3468b1f44a28b66c61e1ebafec
blindcosmos <104645183+blindcosmos@users.noreply.github.com>
2022-05-24 11:40:35 -0300

    New global variables

--- ggn/__init__.py
+++ ggn/__init__.py
@@ -5,5 +5,9 @@ import os
 # minimum value allow in diagonal for ensuring positive definitiveness
 PD_THR = 0.01
 
-basename = os.path.dirname(__file__)
-TOYGRAPH = os.path.join(basename, 'toy.npz')
+# toygraph data, used only for usage examples
+basename       = os.path.dirname(__file__)
+TOYGRAPH       = os.path.join(basename, 'toy.npz')
+TOYGRAPH_means = os.path.join(basename, 'TOYGRAPH_means.npy')
+TOYGRAPH_diags = os.path.join(basename, 'TOYGRAPH_diags.npy')
+TOYGRAPH_covms = os.path.join(basename, 'TOYGRAPH_covms.npy')
